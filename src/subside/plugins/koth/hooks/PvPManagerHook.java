644c16e82e0503f790dee14c7634134793543e61
Thomas 'SubSide' van den Bulk <Thomasvdbulk@gmail.com>
2017-03-12 19:54:48 +0100

    Added: PvPManager premium support
    This change will make the plugin also work for the premium version of
    PvPManager
    
    Also fixed some unnessesary reference in the Vanish Hook when it is
    disabled.

--- src/subside/plugins/koth/hooks/PvPManagerHook.java
+++ src/subside/plugins/koth/hooks/PvPManagerHook.java
@@ -7,12 +7,10 @@ import org.bukkit.entity.Player;
 
 import lombok.Getter;
 import lombok.Setter;
-import me.NoChance.PvPManager.PvPManager;
-import me.NoChance.PvPManager.Managers.PlayerHandler;
+import me.NoChance.PvPManager.PvPlayer;
 
 public class PvPManagerHook extends AbstractHook {
     private @Getter @Setter boolean enabled = false;
-    private PlayerHandler playerHandler;
     
     public PvPManagerHook(HookManager hookManager){
         super(hookManager); // First call the constructor of the parent class
@@ -21,14 +19,13 @@ public class PvPManagerHook extends AbstractHook {
             if(getPlugin().getConfigHandler().getHooks().isPvpManager()){
                 enabled = true;
             }
-            playerHandler = ((PvPManager)Bukkit.getServer().getPluginManager().getPlugin("PvPManager")).getPlayerHandler();
         }
         getPlugin().getLogger().log(Level.INFO, "PvPManager hook: "+(enabled?"Enabled":"Disabled"));
     }
     
     @Override
     public boolean canCap(Player player) {
-        return !playerHandler.get(player).isNewbie();
+        return !PvPlayer.get(player).isNewbie();
     }
 
 }
